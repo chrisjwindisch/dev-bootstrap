109b84aee6f7edaa3b35bca2dc6ff87faae0893b
C.J. Windisch <chrisjwindisch@gmail.com>
2022-07-16 15:10:31 -0700

    Creating firebase project

--- tools/init.sh
+++ tools/init.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# globals
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 # utilities
 
 # courtesy: https://stackoverflow.com/a/14203146/10302179
 while [[ $# -gt 0 ]]; do
   case $1 in
     -n|--name)
-      PROJECT_NAME="$2"
+      PROJECT_NAME=`echo "$2" | sed -e 's/^[[:space:]]*//'` # trim the name
       REPO_NAME=$(echo $PROJECT_NAME | tr " " "-" | tr '[:upper:]' '[:lower:]')
+      GCLOUD_PROJECT_NAME=$PROJECT_NAME
+      RANDOM_STRING=$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-8)
+      GCLOUD_PROJECT_ID="$REPO_NAME-$RANDOM_STRING"
       shift # past argument
       shift # past value
       ;;
@@ -22,7 +28,20 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+if [ -z "$GCLOUD_PROJECT_NAME" ]
+then
+      echo "Please pass in a name with the -n somename"
+      exit
+fi
+
 # init a new project
 # args: --name # project name in natural case with spaces. dev references to project will remove spaces and make lowercase
 # Create a github repo for the project
 gh repo create $REPO_NAME -c --private
+# Setup Firebase
+gcloud auth login # Triggers a browser login
+FIREBASE_TOKEN=$(firebase login:ci | grep '1//' | cat | sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g")
+echo $SCRIPT_PATH/firebase.exp "$FIREBASE_TOKEN" "$GCLOUD_PROJECT_NAME" "$GCLOUD_PROJECT_ID"
+expect $SCRIPT_PATH/firebase.exp "$FIREBASE_TOKEN" "$GCLOUD_PROJECT_NAME" "$GCLOUD_PROJECT_ID"
+#firebase init --token $FIREBASE_TOKEN hosting # Works but goes into interactive mode
+#gcloud projects create $GCLOUD_PROJECT_ID --name=$GCLOUD_PROJECT_NAME
--- tools/osx/setup.sh
+++ tools/osx/setup.sh
@@ -2,17 +2,10 @@
 
 # utilities
 
-# courtesy: https://apple.stackexchange.com/a/425118/378721
-brew_install() {
-    echo "\nInstalling $1"
-    if brew list $1 &>/dev/null; then
-        echo "${1} is already installed"
-    else
-        brew install $1 && echo "$1 is installed"
-    fi
-}
-
 # ---------------------------------------------------------------
 
 # setup tools needed to run a project on a new laptop
-brew_install gh # install github tools
+brew install gh # install github tools
+brew install --cask google-cloud-sdk # install google-cloud sdk
+brew install firebase-cli
+brew install md5sha1sum
