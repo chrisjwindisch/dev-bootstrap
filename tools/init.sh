0a910e94b6ba28f2b06f07e9ddbdbcdb05d88efc
C.J. Windisch <chrisjwindisch@gmail.com>
2022-07-22 20:19:09 -0700

    Fixed project gcloud project id that needed to be all lowercase

--- tools/init.sh
+++ tools/init.sh
@@ -8,18 +8,24 @@ SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 # courtesy: https://stackoverflow.com/a/14203146/10302179
 while [[ $# -gt 0 ]]; do
   case $1 in
+    -h|--help)
+      echo "-h | --help: This help menu"
+      echo "-n | --name: (Required) The name of your project. Must be less than 21 characters with letters, numbers, and hyphens (Firebase's requirements)"
+      exit 0
+      ;;
     -n|--name)
       PROJECT_NAME=`echo "$2" | sed -e 's/^[[:space:]]*//'` # trim the name
       REPO_NAME=$(echo $PROJECT_NAME | tr " " "-" | tr '[:upper:]' '[:lower:]')
       GCLOUD_PROJECT_NAME=$PROJECT_NAME
       RANDOM_STRING=$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-8)
-      GCLOUD_PROJECT_ID="$REPO_NAME-$RANDOM_STRING"
+      # Req's for Firebase Project id https://cloud.google.com/resource-manager/docs/creating-managing-projects
+      GCLOUD_PROJECT_ID=$(echo "$REPO_NAME-$RANDOM_STRING" | tr "[:upper:]" "[:lower:]")
       shift # past argument
       shift # past value
       ;;
     -*|--*)
       echo "Unknown option $1"
-      exit 1
+      exit 5
       ;;
     *)
       POSITIONAL_ARGS+=("$1") # save positional arg
@@ -31,7 +37,8 @@ done
 if [ -z "$GCLOUD_PROJECT_NAME" ]
 then
       echo "Please pass in a name with the -n somename"
-      exit
+      echo "--help for help"
+      exit 5
 fi
 
 # init a new project
@@ -45,3 +52,5 @@ echo $SCRIPT_PATH/firebase.exp "$FIREBASE_TOKEN" "$GCLOUD_PROJECT_NAME" "$GCLOUD
 expect $SCRIPT_PATH/firebase.exp "$FIREBASE_TOKEN" "$GCLOUD_PROJECT_NAME" "$GCLOUD_PROJECT_ID"
 #firebase init --token $FIREBASE_TOKEN hosting # Works but goes into interactive mode
 #gcloud projects create $GCLOUD_PROJECT_ID --name=$GCLOUD_PROJECT_NAME
+
+exit 0
