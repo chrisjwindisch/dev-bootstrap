d0288b24ea4e34980fb43b93c08cfa986790ffa4
C.J. Windisch <chrisjwindisch@gmail.com>
2022-07-23 10:36:20 -0700

    Setup for node, npm, yarn

--- tools/osx/setup.sh
+++ tools/osx/setup.sh
@@ -6,6 +6,8 @@
 
 # setup tools needed to run a project on a new laptop
 brew install gh # install github tools
+brew install node
+npm install --global yarn
 brew install --cask google-cloud-sdk # install google-cloud sdk
 brew install firebase-cli
 brew install md5sha1sum
